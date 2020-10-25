3b4ad2dbe281e49f04e3b62a55f06062319aaf44
bonasunu <bonaventurasunu@gmail.com>
2020-10-25 16:33:39 +0700

    add server to project

---
+++ app.js
@@ -0,0 +1,8 @@
+const { urlencoded } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+module.exports = app
